07163a36af333060a353364346cdf7d7320b3a2c
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-10 16:23:28 +0530

    coaches card done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,85 +2,22 @@
 // REACT //
 import React from "react";
 
-// ENUMS //
-import { Colors, Variants } from "@/neevo/enums/core.enum";
-import { ButtonLevels, ButtonSizes } from "@/neevo/enums/button.enum";
-
-// STYLES //
-import styles from "@/app/page.module.scss";
-
 // COMPONENTS //
-import Image from "next/image";
-import Button from "@/neevo/components/button/Button";
-
-// IMAGES //
-import Pixolo from "@/../public/images/pixolo.png";
-import BrandLogo from "@/../public/images/brand/brand-logo.png";
+import CoachesCard from "@/components/coaches-card/CoachesCard";
 
 /** Home Screen */
 const HomeScreen: React.FC<unknown> = () => {
 	return (
-		<div className={styles.homeContainer}>
-			{/* Neevo Icon */}
-			<Image
-				className={styles.brandIcon}
-				src={BrandLogo}
-				alt="Neevo Logo"
-				width={210}
+		<div className={"flex justify-center align-center"}>
+			<CoachesCard
+				first_name={"Shubam"}
+				last_name={"Pandit"}
+				description={
+					"Pandit is our Head Coach, he is very experienced and good with kids. He has school experience. He is thin. He wears specs."
+				}
+				designation={"Head coach"}
+				src={"/images/pandit.png"}
 			/>
-
-			<div className={styles.homeContent}>
-				{/* Heading */}
-				<p className={styles.homeHeading}> Build with Precision </p>
-				{/* Subheading */}
-				<p className={styles.homeSubheading}>
-					Empowering Teams to <span className={styles.designText}>Design</span> with
-					Purpose
-				</p>
-			</div>
-
-			<div>
-				{/* Explore More */}
-				<p className={styles.exploreMore}>Explore more</p>
-				<div className={styles.actionButtons}>
-					{/* Documentation */}
-					<Button
-						text="Docs"
-						onClick={() => console.log("clicked")}
-						size={ButtonSizes.LARGE}
-						variant={Variants.SOFT}
-						color={Colors.PRIMARY}
-						level={ButtonLevels.INLINE}
-					/>
-
-					{/* Login */}
-					<Button
-						text="Login"
-						onClick={() => console.log("clicked")}
-						size={ButtonSizes.LARGE}
-						variant={Variants.SOFT}
-						color={Colors.PRIMARY}
-						level={ButtonLevels.INLINE}
-					/>
-
-					{/* Setting */}
-					<Button
-						text="Settings"
-						onClick={() => console.log("clicked")}
-						size={ButtonSizes.LARGE}
-						variant={Variants.SOFT}
-						color={Colors.PRIMARY}
-						level={ButtonLevels.INLINE}
-					/>
-				</div>
-			</div>
-
-			{/* Footer */}
-			<div className={styles.homeBottomContainer}>
-				<p className={styles.bottomText}>Made by developers at</p>
-				{/* Pixolo Logo */}
-				<Image className={styles.pixoloIcon} src={Pixolo} alt="Pixolo" />
-			</div>
 		</div>
 	);
 };
--- src/components/coaches-card/CoachesCard.tsx
+++ src/components/coaches-card/CoachesCard.tsx
@@ -1,22 +1,34 @@
 "use client";
+// REACT //
 import React from "react";
 
+// ENUMS //
+import { Colors, Shapes, Sizes } from "@/neevo/enums/core.enum";
+
 // STYLES //
+import styles from "./coaches-card.module.scss";
 
 // COMPONENTS //
+import Image from "next/image";
+import Icon from "@/neevo/components/Icon";
+import Chip from "@/neevo/components/chip/Chip";
 
-// IMAGES //
-
-// SERVICES //
-
-// TYPES //
-
-// UTILS //
-
-// PLUGINS //
+interface CoachesCardProps {
+	first_name: string;
+	last_name: string;
+	description: string;
+	designation: string;
+	src: string;
+}
 
 /** Coaches Card Screen */
-const CoachesCard: React.FC<unknown> = () => {
+const CoachesCard: React.FC<CoachesCardProps> = ({
+	first_name = "",
+	last_name = "",
+	description = "",
+	designation = "",
+	src = "",
+}) => {
 	// Navigation and Route Params
 
 	// Define States
@@ -28,7 +40,39 @@ const CoachesCard: React.FC<unknown> = () => {
 	// UseEffect Functions and UseFocusEffect Functions
 
 	// View starts here
-	return <div>Hello Coaches Card</div>;
+	return (
+		<div className={styles.contentWrapper}>
+			{/* Card title */}
+			<div className={styles.titleWrapper}>
+				{/* Coach First name */}
+				<p className={styles.firstName}>{first_name}</p>
+				{/* Coach Last name */}
+				<p className={styles.lastName}>{last_name}</p>
+			</div>
+			<div className={styles.imageDescriptionWrapper}>
+				{/* Image */}
+				<div className={styles.imageWrapper}>
+					<Image src={src} alt="img" width={280} height={280} />
+				</div>
+				{/* Description */}
+				<p className={styles.description}>{description}</p>
+			</div>
+			<button className={`${styles.linkButton} flex align-center justify-center`}>
+				{/* Icon */}
+				{/* TODO: Add Social media icon */}
+				<Icon className={styles.buttonIcon} iconName={"arrow"} />
+			</button>
+			<div className={styles.chipWrapper}>
+				{/* Designation */}
+				<Chip
+					text={designation}
+					color={Colors.PRIMARY}
+					shape={Shapes.ROUNDED}
+					size={Sizes.LARGE}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default CoachesCard;
