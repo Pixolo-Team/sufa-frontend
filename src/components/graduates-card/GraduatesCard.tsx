9b21c88ddf0e4db7b74dc612562c756932340593
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-10 18:04:00 +0530

    graduates card component done

--- src/components/graduates-card/GraduatesCard.tsx
+++ src/components/graduates-card/GraduatesCard.tsx
@@ -1,22 +1,25 @@
 "use client";
+// REACT //
 import React from "react";
 
 // STYLES //
+import styles from "./graduates-card.module.scss";
 
 // COMPONENTS //
+import Image from "next/image";
 
-// IMAGES //
-
-// SERVICES //
-
-// TYPES //
-
-// UTILS //
-
-// PLUGINS //
+interface GraduatesCardProps {
+	src: string;
+	coach_name: string;
+	description: string;
+}
 
 /** Graduates Card Screen */
-const GraduatesCard: React.FC<unknown> = () => {
+const GraduatesCard: React.FC<GraduatesCardProps> = ({
+	src = "",
+	coach_name = "",
+	description = "",
+}) => {
 	// Navigation and Route Params
 
 	// Define States
@@ -28,7 +31,21 @@ const GraduatesCard: React.FC<unknown> = () => {
 	// UseEffect Functions and UseFocusEffect Functions
 
 	// View starts here
-	return <div>Hello Graduates Card</div>;
+	return (
+		// Card Component
+		<div className={styles.cardWrapper}>
+			<div className={styles.imageWrapper}>
+				{/* Image */}
+				<Image src={src} alt="img" width={260} height={360} />
+			</div>
+			<div className={styles.textWrapper}>
+				{/* Coach name */}
+				<p className={styles.title}>{coach_name}</p>
+				{/* Description */}
+				<p className={styles.description}>{description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default GraduatesCard;
