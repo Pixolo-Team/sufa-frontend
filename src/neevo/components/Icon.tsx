784aa88ab59323a9ad22b2629c940909c3db5f8e
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-11 16:22:01 +0530

    minor fix

--- src/neevo/components/Icon.tsx
+++ src/neevo/components/Icon.tsx
@@ -1,4 +1,4 @@
-// SVG's //
+// SVGs //
 import ArrowFilled from "@/../public/icons/filled/arrow.svg";
 import BoxMenuFilled from "@/../public/icons/filled/box-menu.svg";
 import CloseFilled from "@/../public/icons/filled/close.svg";
@@ -16,6 +16,7 @@ import MailOpenFilled from "@/../public/icons/filled/mail-open.svg";
 import MoreOptionsFilled from "@/../public/icons/filled/more-options.svg";
 import RightArrowFilled from "@/../public/icons/filled/right-arrow.svg";
 import SearchFilled from "@/../public/icons/filled/search.svg";
+//TODO : Add SearchOptionFilled
 import UserCircleFilled from "@/../public/icons/filled/user-circle.svg";
 import UsersFilled from "@/../public/icons/filled/users.svg";
 import VerfiedFilled from "@/../public/icons/filled/verified.svg";
@@ -52,8 +53,6 @@ import PdfOutline from "@/../public/icons/outline/pdf.svg";
 import UploadTrayOutline from "@/../public/icons/outline/upload-tray.svg";
 import InvisibleOutline from "@/../public/icons/outline/invisible.svg";
 import DeleteOutline from "@/../public/icons/outline/delete.svg";
-
-//TODO : Add SearchOptionFilled
 
 // Define the mapping of component names to components
 const filled: {
