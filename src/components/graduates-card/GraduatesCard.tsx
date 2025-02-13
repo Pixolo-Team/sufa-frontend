fe145cf1815aed3286f04a5c84847d18b874ce57
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-13 17:52:27 +0530

    pr fixes

--- src/components/graduates-card/GraduatesCard.tsx
+++ src/components/graduates-card/GraduatesCard.tsx
@@ -9,15 +9,15 @@ import styles from "./graduates-card.module.scss";
 import Image from "next/image";
 
 interface GraduatesCardProps {
-	src: string;
-	coach_name: string;
+	graduateImgSrc: string;
+	graduateName: string;
 	description: string;
 }
 
 /** Graduates Card Screen */
 const GraduatesCard: React.FC<GraduatesCardProps> = ({
-	src = "",
-	coach_name = "",
+	graduateImgSrc = "",
+	graduateName = "",
 	description = "",
 }) => {
 	// Navigation and Route Params
@@ -33,14 +33,22 @@ const GraduatesCard: React.FC<GraduatesCardProps> = ({
 	// View starts here
 	return (
 		// Card Component
-		<div className={`${styles.cardWrapper} flex justify-center align∂-center`}>
+		<div
+			className={`${styles.cardWrapper} flex justify-center align-center flex-column`}
+		>
 			<div className={styles.imageWrapper}>
 				{/* Image */}
-				<Image src={src} alt="img" width={260} height={360} />
+				<Image
+					src={graduateImgSrc}
+					className={styles.image}
+					alt={graduateName}
+					width={260}
+					height={360}
+				/>
 			</div>
 			<div className={styles.textWrapper}>
 				{/* Coach name */}
-				<p className={styles.title}>{coach_name}</p>
+				<p className={styles.title}>{graduateName}</p>
 				{/* Description */}
 				<p className={styles.description}>{description}</p>
 			</div>
