846fd9ee3b700de26f5f1722cb89987d53b5b999
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-14 12:45:21 +0530

    pr fixes

--- src/sections/home/founders-message/FoundersMessage.tsx
+++ src/sections/home/founders-message/FoundersMessage.tsx
@@ -11,8 +11,8 @@ import Icon from "@/neevo/components/Icon";
 import ContentFromCms from "@/components/cms/ContentFromCms";
 
 // IMAGES //
-import FounderImage from "../../../../public/images/sir.jpg";
-import FounderSign from "../../../../public/images/founder-sign.png";
+import FounderImage from "@/../public/images/sir.jpg";
+import FounderSign from "@/../public/images/founder-sign.png";
 
 /** Founders Message Screen */
 const FoundersMessage: React.FC<unknown> = () => {
@@ -28,50 +28,53 @@ const FoundersMessage: React.FC<unknown> = () => {
 
 	// View starts here
 	return (
-		// Founders page
 		<div className={`${styles.contentWrapper} flex justify-center align-center`}>
-			<div className={styles.messageWrapper}>
-				{/* Quotes svg */}
-				<div className={styles.doubleQuotes}>
-					<Icon iconName="quote" className={styles.verifiedIcon} mode="filled" />
-					<Icon iconName="quote" className={styles.verifiedIcon} mode="filled" />
+			<div className="left-container-padding">
+				<div className={styles.messageWrapper}>
+					{/* Quotes svg */}
+					<div className={`${styles.doubleQuotes} flex justify-center`}>
+						<Icon iconName="quote" className={styles.verifiedIcon} mode="filled" />
+						<Icon iconName="quote" className={styles.verifiedIcon} mode="filled" />
+					</div>
+					{/* Founder message */}
+					<ContentFromCms wrapperClassName={styles.founderContentStyles}>
+						<p>
+							At Skorost United Academy, every child who steps onto the field isn’t
+							just a player—we see them as the future of the game, and more
+							importantly, the future of life itself. This academy is built on passion,
+							discipline, and an unbreakable spirit—the same values that have shaped my
+							own journey in football.
+						</p>
+						<p>
+							We don’t just teach football; we build character. We create an
+							environment where young athletes grow into strong, confident
+							individuals—ready to take on challenges, both on and off the field. Every
+							drill, every match, every lesson is designed not just to make better
+							players, but to make better people.
+						</p>
+						<p>
+							Here, you’re not just joining an academy—you’re becoming part of a
+							legacy. Welcome to Skorost United. Let’s write history together.
+						</p>
+					</ContentFromCms>
+					{/* Founder sign */}
+					<Image src={FounderSign} alt="sign" />
+					{/* Founder Name */}
+					<p className={`${styles.founderName} font-weight-800`}>ABHAY AMIN</p>
+					{/* About */}
+					<p className={`${styles.designation} font-weight-700`}>FOUNDER & OWNER</p>
 				</div>
-				{/* Founder message */}
-				<ContentFromCms wrapperClassName={styles.wrapperStyles}>
-					<p>
-						At Skorost United Academy, every child who steps onto the field isn’t just
-						a player—we see them as the future of the game, and more importantly, the
-						future of life itself. This academy is built on passion, discipline, and
-						an unbreakable spirit—the same values that have shaped my own journey in
-						football.
-					</p>
-					<p>
-						We don’t just teach football; we build character. We create an environment
-						where young athletes grow into strong, confident individuals—ready to take
-						on challenges, both on and off the field. Every drill, every match, every
-						lesson is designed not just to make better players, but to make better
-						people.
-					</p>
-					<p>
-						Here, you’re not just joining an academy—you’re becoming part of a legacy.
-						Welcome to Skorost United. Let’s write history together.
-					</p>
-				</ContentFromCms>
-				{/* Founder sign */}
-				<Image src={FounderSign} alt="sign" />
-				{/* Founder Name */}
-				<p className={`${styles.founderName} font-weight-800`}>ABHAY AMIN</p>
-				{/* About */}
-				<p className={`${styles.about} font-weight-700`}>FOUNDER & OWNER</p>
 			</div>
 			{/* Founder Image */}
-			<Image
-				src={FounderImage}
-				alt="founder"
-				width={500}
-				height={500}
-				className={styles.founderImage}
-			/>
+			<div className={styles.imageWrapper}>
+				<Image
+					src={FounderImage}
+					alt="founder"
+					width={500}
+					height={500}
+					className={styles.founderImage}
+				/>
+			</div>
 		</div>
 	);
 };
