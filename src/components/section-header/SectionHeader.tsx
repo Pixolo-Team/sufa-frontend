4c6854653c2f0e8c31a13fea2c1f854bacf21616
omisan01 <omkar12mitake@gmail.com>
2025-02-15 18:56:24 +0530

    pr fixes

--- src/components/section-header/SectionHeader.tsx
+++ src/components/section-header/SectionHeader.tsx
@@ -41,7 +41,7 @@ const SectionHeader: React.FC<SectionHeaderProps> = ({
 					<Image
 						src={leftImage}
 						alt="Section Header Image"
-						className={`${styles.image} img-responsive full-width-images`}
+						className={`${styles.image} img-responsive full-width-image`}
 						width={100}
 						height={100}
 					/>
