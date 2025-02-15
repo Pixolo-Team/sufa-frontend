ca0c7a57983bfa8753489d49c7fdad6751dc7e7f
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-15 20:45:52 +0530

    graduates section complete

--- src/sections/home/graduates/Graduates.tsx
+++ src/sections/home/graduates/Graduates.tsx
@@ -1,20 +1,61 @@
 "use client";
-import React from "react";
+// REACT //
+import React, { useRef } from "react";
+import Slider, { Settings } from "react-slick";
 
 // STYLES //
+import styles from "./graduates.module.scss";
 
 // COMPONENTS //
+import CustomSlickArrows from "@/components/custom-slick-arrows/CustomSlickArrows";
+import GraduatesCard from "@/components/graduates-card/GraduatesCard";
+import SectionHeader from "@/components/section-header/SectionHeader";
 
-// IMAGES //
-
-// SERVICES //
-
-// TYPES //
-
-// UTILS //
-
-// PLUGINS //
-
+// Slider settings
+const settings: Settings = {
+	dots: false,
+	arrows: false,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: false,
+	autoplaySpeed: 3000,
+	variableWidth: true,
+	centerMode: true,
+	responsive: [
+		{
+			breakpoint: 992, // iPad Air
+			settings: { slidesToShow: 2 },
+		},
+		{
+			breakpoint: 600,
+			settings: { slidesToShow: 1, variableWidth: false },
+		},
+	],
+};
+const graduatesList = [
+	{
+		description: "Graduated in 2003 and played in the India League for DK Pharma",
+		graduateName: "Ravi Shankar",
+		graduateSrc: "/images/graduate.jpg",
+	},
+	{
+		description: "Graduated in 2003 and played in the India League for DK Pharma",
+		graduateName: "Ravi Shankar",
+		graduateSrc: "/images/graduate.jpg",
+	},
+	{
+		description: "Graduated in 2003 and played in the India League for DK Pharma",
+		graduateName: "Ravi Shankar",
+		graduateSrc: "/images/graduate.jpg",
+	},
+	{
+		description: "Graduated in 2003 and played in the India League for DK Pharma",
+		graduateName: "Ravi Shankar",
+		graduateSrc: "/images/graduate.jpg",
+	},
+];
 /** Graduates Screen */
 const Graduates: React.FC<unknown> = () => {
 	// Navigation and Route Params
@@ -22,13 +63,39 @@ const Graduates: React.FC<unknown> = () => {
 	// Define States
 
 	// Define Refs
+	const sliderRef = useRef<Slider | null>(null);
 
 	// Helper Functions
 
 	// UseEffect Functions and UseFocusEffect Functions
 
 	// View starts here
-	return <div>Hello Graduates</div>;
+	return (
+		<section className="section-spacing">
+			{/* Section Header */}
+			<SectionHeader fadedText="Champs" highlightedText="Meet Our Graduates" />
+			{/* Graduate slider */}
+			<div className={`${styles.graduatesWrapper}`}>
+				<Slider ref={sliderRef} {...settings} className={styles.slider}>
+					{graduatesList.map((graduate, graduateIndex) => (
+						<div key={graduateIndex}>
+							<div className={styles.cardWrap}>
+								<div className={styles.cardWrapInner}>
+									<GraduatesCard
+										description={graduate.description}
+										graduateName={graduate.graduateName}
+										graduateImgSrc={graduate.graduateSrc}
+									/>
+								</div>
+							</div>
+						</div>
+					))}
+				</Slider>
+				{/* Custom slick arrows */}
+				<CustomSlickArrows sliderRef={sliderRef} />
+			</div>
+		</section>
+	);
 };
 
 export default Graduates;
