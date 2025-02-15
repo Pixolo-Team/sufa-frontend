a1d0b54ed5ddb3489dcd1517c7408fec2dfe1e75
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-15 11:59:52 +0530

    pr fixes

--- src/sections/home/courses/Courses.tsx
+++ src/sections/home/courses/Courses.tsx
@@ -26,11 +26,11 @@ const Courses: React.FC<unknown> = () => {
 		<div>
 			{/* Section header component */}
 			<SectionHeader fadedText="Champions" highlightedText="Courses" />
-			<p className={`${styles.sectionDescription} font-weight-500`}>
+			<p className={`${styles.sectionDescription} font-weight-500 text-center`}>
 				Designed for Excellence!
 			</p>
 			{/* Courses card components */}
-			<div className={`${styles.cardsWrapper} container`}>
+			<div className={`${styles.cardsWrapper} container flex flex-column`}>
 				<CoursesCard
 					courseImageSrc="/images/keeper.jpg"
 					courseTitle="Goalkeeper Development"
