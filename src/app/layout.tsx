84c2ef4c49c1797edaa08c3d6fedda00e94f66e0
pixolopr <contactabhay2@gmail.com>
2025-02-17 02:58:37 +0530

    Removed New Line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,6 @@ export default function RootLayout({
 						>
 							{/* Header component */}
 							<Header />
-							
 							{/* Main content */}
 							<main className="main">{children}</main>
 
