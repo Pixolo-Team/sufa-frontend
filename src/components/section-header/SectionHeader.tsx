3b47609e1b0e22d61d4559c8b55b9efdfea320c9
Adarsh Anchan <anchanadarsh@gmail.com>
2025-02-17 20:51:54 +0530

    section header normal animation added

--- src/components/section-header/SectionHeader.tsx
+++ src/components/section-header/SectionHeader.tsx
@@ -33,7 +33,7 @@ const SectionHeader: React.FC<SectionHeaderProps> = ({
 	// View starts here
 	return (
 		<div
-			className={`section-header-parallax text-center flex align-center justify-center ${styles.wrapper}`}
+			className={`fade-in-up text-center flex align-center justify-center ${styles.wrapper}`}
 		>
 			{/* Image */}
 			{!!leftImage && (
