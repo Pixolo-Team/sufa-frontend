482d061c40cc36facf04a616e71569262ca37382
MohdhussainSunesara <m.sunesara@somaiya.edu>
2025-02-17 14:12:57 +0530

    header complete

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,20 @@
 "use client";
-import React from "react";
+// REACT //
+import React, { useState } from "react";
 
 // STYLES //
 import styles from "./header.module.scss";
 
-// COMPONENTS //
-
-// IMAGES //
-
-// SERVICES //
-
-// TYPES //
-
-// UTILS //
-
-// PLUGINS //
+// SVG's //
+import SkorostLogo from "@/../public/images/skorost.svg";
+import HamburgerIcon from "@/../public/icons/hamburger.svg";
 
 /** Header Screen */
 const Header: React.FC<unknown> = () => {
 	// Navigation and Route Params
 
 	// Define States
+	const [isDropdownVisible, setDropdownVisible] = useState<boolean>(false);
 
 	// Define Refs
 
@@ -29,7 +23,35 @@ const Header: React.FC<unknown> = () => {
 	// UseEffect Functions and UseFocusEffect Functions
 
 	// View starts here
-	return <header className={styles.headerWrap}>Hello Header</header>;
+	return (
+		<header className={styles.headerWrap}>
+			<div className="flex justify-between align-center">
+				{/* Header logo */}
+				<SkorostLogo />
+				{/* Titles */}
+				<div className={`${styles.titleWrapper} font-weight-500 justify-end`}>
+					<p>Courses</p>
+					<p>Contact US</p>
+				</div>
+				{/* Hamburger Menu */}
+				<div
+					className={styles.hamburgerMenu}
+					onClick={() => setDropdownVisible((prev) => !prev)}
+				>
+					<HamburgerIcon />
+				</div>
+			</div>
+			{/* Menu Dropdown */}
+			{isDropdownVisible && (
+				<div
+					className={`${styles.dropdownWrapper} font-weight-500 align-start flex-column`}
+				>
+					<p>Courses</p>
+					<p>Contact US</p>
+				</div>
+			)}
+		</header>
+	);
 };
 
 export default Header;
