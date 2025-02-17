573b79beb1a34e9dc77074c62c2f012de7b0c3fa
omisan01 <omkar12mitake@gmail.com>
2025-02-17 13:44:44 +0530

    lint fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({
 						>
 							{/* Header component */}
 							<Header />
-							
+
 							{/* Main content */}
 							<main className="main">{children}</main>
 
