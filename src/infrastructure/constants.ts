30d72604f535bfa357308e434aab4ed4c17ee83c
omisan01 <omkar12mitake@gmail.com>
2025-06-04 18:04:07 +0530

    changed api url

--- src/infrastructure/constants.ts
+++ src/infrastructure/constants.ts
@@ -2,7 +2,7 @@ export const CONSTANTS = {
 	// Company Name
 	COMPANY_NAME: "Skorost United Football Academy",
 	// API URL
-	API_URL: "http://3.108.225.0:3005/api/skorost",
+	API_URL: "https://api.skorostunited.com/api/skorost",
 	// Brand short name
 	COMPANY_SHORT_NAME: "pixolo",
 };
